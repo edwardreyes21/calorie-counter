4a62b2533f7ad29cc91c3ca391ebb1f6b9b5def8
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-02 18:07:01 -0800

    Changes TDEE based on selected activity level

--- src/ActivityForm.js
+++ src/ActivityForm.js
@@ -1,8 +1,23 @@
 import { useState } from 'react';
 
 function ActivityForm(props) {
+    const [multiplier, setMultiplier] = useState(1);
+
+    const handleMultiplierChange = (event) => {
+        setMultiplier(event.target.value);
+    }
+
     return (
-        <p>TDEE: {props.tdee.toFixed(2)}</p>
+        <div className="ActivityForm">
+            <p>TDEE: {(props.tdee * multiplier).toFixed(2)}</p>
+            <form>
+                <label for="choices">Choose one:</label>
+                <select id="choices" name="choices" value={multiplier} onChange={handleMultiplierChange}>
+                    <option value="1.0">Basal Metabolic Rate (1.0)</option>
+                    <option value="1.2">Sedentary (1.2)</option>
+                </select>
+            </form>
+        </div>
     );
 }
 
