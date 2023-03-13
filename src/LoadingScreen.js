80d2abbc28b01f1b1bebc041e49563be153bca07
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-12 22:46:54 -0700

    Fixed bug and changed properties of loading screen

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -2,15 +2,15 @@ import { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
 function LoadingScreen(props) {
-    const style = useEffect({
+    const style = useSpring({
         to: {opacity: 1},
         from: {opacity: 0},
-        delay: 1000
+        delay: 300
     });
 
     return (
         <animated.div style={style}>
-            <h1>Calorie Counter loading..</h1>
+            <h1>Edward's Calorie Counter</h1>
         </animated.div>
     );
 };
