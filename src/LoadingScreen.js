16cc1e475e7988a492eb061d6653bc8b234e52d4
edwardreyes21 <edwardi.reyes21@gmail.com>
2023-03-12 22:21:57 -0700

    Added styling to loading screen component

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react';
 
 function LoadingScreen(props) {
+    const style = useEffect({
+        to: {opacity: 1},
+        from: {opacity: 0},
+        delay: 1000
+    });
 
-}
+    return (
+        <animated.div style={style}>
+            <h1>Calorie Counter loading..</h1>
+        </animated.div>
+    );
+};
 
 export default LoadingScreen;
